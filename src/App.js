5526869de494c87225e46c2b49843b53ee10c067
久保田公平 <kubotakouhei@kubotakouheinoMacBook-puro.local>
2019-07-25 16:41:29 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  return <div><h1>Hello World</h1></div>;
+  const greeting = 'Hi, Tom!'
+  const dom = <h1 className='foo'>{greeting}</h1>
+  return dom;
 }
 
 export default App;
