14773376179642d4f6be76f3d7d62cec79bea0ed
久保田公平 <kubotakouhei@kubotakouheinoMacBook-puro.local>
2019-07-28 18:41:27 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-function App() {
-  const greeting = 'Hi, Tom!'
-  const dom = <h1 className='foo'>{greeting}</h1>
-  return dom;
+// function App() {
+//   const greeting = 'Hi, Tom!'
+//   const dom = <h1 className='foo'>{greeting}</h1>
+//   return dom;
+// }
+
+const App = () => {
+  const profiles = [
+    { name: 'Taro', age: 10 },
+    { name: 'Hanako', age: 5 },
+    { name: 'Ken'}
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
+  )
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
