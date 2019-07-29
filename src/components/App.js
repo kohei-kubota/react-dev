1ea97f48beb494cc9154f9ac6ea7b6b8e4ba469a
久保田公平 <kubotakouhei@kubotakouheinoMacBook-puro.local>
2019-07-29 21:11:00 +0900

    implement component with connect

--- src/components/App.js
+++ src/components/App.js
@@ -7,34 +7,31 @@ import React, {Component} from 'react';
 //   return dom;
 // }
 
-const App = () => (<Counter></Counter>)
+import {connect} from 'react-redux'
+import {increment, decrement} from '../actions'
 
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    console.log(this.state)
-    this.state = {count:0}
-  }
+// const App = () => (<Counter></Counter>)
 
-  handlePlusButton = () => {
-    // const count = this.state.count
-    this.setState({count: this.state.count + 1})
-  }
-
-  handleMinusButton = () => {
-    this.setState({count: this.state.count - 1})
-  }
-
+class App extends Component {
   render() {
-    console.log(this.state)
+    const props = this.props
     return (
       <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value })
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
+const mapDispatchToProps = ({increment, decrement})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+// export default App;
